019f3c93ebb7105bde7fc5fc000e6ab80b56c8eb
erikjacso <jarik93@gmail.com>
2016-04-24 23:15:25 +0200

    Update Test9.java

--- src/main/Test9.java
+++ src/main/Test9.java
@@ -8,7 +8,20 @@ public class Test9 {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		GameField gameField = new GameField();
+		gameField.Initialize(0);
+		int i=0;
+		
+		int nonSpecialX=4;
+		int nonSpecialY=0;
+		
+		gameField.getPlayer().moveTo(gameField.getCell(gameField.getPlayer().getPosition().getX()+1,gameField.getPlayer().getPosition().getY()), Direction.RIGHT);
+		gameField.getPlayer().shootFirstPortal(Color.BLUE);
+		assertTrue(gameField.getCell(nonSpecialX, nonSpecialY) instanceof Wall);
+		assertFalse(gameField.getCell(nonSpecialX, nonSpecialY) instanceof SpecialWall);
+		assertEquals(gameField.getCell(nonSpecialX,nonSpecialY).getImage(),'|');
+
+		
 	}
 
 }
