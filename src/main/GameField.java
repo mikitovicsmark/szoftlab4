043576bb18a9262aaef56d63f544cea037adb1d1
Erik <Erik@Erik-PC>
2016-04-24 11:51:53 +0200

    Implemented switch and door input handling, STILL BUGGY DON'T MERGE

--- src/main/GameField.java
+++ src/main/GameField.java
@@ -1,118 +1,145 @@
 package main;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 public class GameField {
-	private int level;	
-	private Player player;
-	private List<List<Cell>> cells = new ArrayList<List<Cell>>();
-	private int height;
-	private int width;
-	private Exit exit;
-	private int zpmCount;
-
-	public Player getPlayer() {
-		return player;
-	}
-
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-
-	public void Initialize(int startLevel) {
-		//reset map
-		cells = new ArrayList<List<Cell>>();
-		level = startLevel;
-		List<String> stringLines = new Serializer().loadMap(level);
-		width = stringLines.get(0).length();
-		height = stringLines.size();
-		zpmCount = 0;
-
-		for (int i = 0; i < height; i++) {
-			List<Cell> line = new ArrayList<Cell>();
-			for (int j = 0; j < width; j++) {
-				char nextCell = stringLines.get(i).charAt(j);
-				switch (nextCell) {
-				case 'Z':
-					zpmCount++;
-					line.add(new NormalFloor(j, i, new Zpm(j, i)));
-					break;
-				case 'B':
-					line.add(new NormalFloor(j, i, new Box(j, i, 5))); // TODO change from fix 5 weight for box
-					break;
-				case 'E':
-					Exit newExit = new Exit(j, i);
-					exit = newExit;
-					line.add(newExit);
-					break;
-				case 'P':
-					line.add(new Pit(j, i));
-					break;
-				case '.':
-					line.add(new NormalFloor(j, i, null));
-					break;
-				case '|':
-					line.add(new Wall(j, i));
-					break;
-				case 'L':
-					line.add(new SpecialWall(j, i));
-					break;
-				case 'O':
-					Cell floor = new NormalFloor(j, i, null);
-					this.player = new Player(floor);
-					this.player.setField(this);
-					line.add(floor);
-					break;
+    private int level; 
+    private Player player;
+    private List<List<Cell>> cells = new ArrayList<List<Cell>>();
+    private int height;
+    private int width;
+    private Exit exit;
+    private int zpmCount;
+ 
+    public Player getPlayer() {
+        return player;
+    }
+ 
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+ 
+    public void Initialize(int startLevel) {
+        //reset map
+        cells = new ArrayList<List<Cell>>();
+        level = startLevel;
+        List<String> stringLines = new Serializer().loadMap(level);
+        width = stringLines.get(0).length();
+        height = stringLines.size();
+        zpmCount = 0;
+ 
+        for (int i = 0; i < height; i++) {
+            List<Cell> line = new ArrayList<Cell>();
+            for (int j = 0; j < width; j++) {
+                char nextCell = stringLines.get(i).charAt(j);
+                switch (nextCell) {
+                case 'S':
+                    int openWeight = java.lang.Character.getNumericValue(stringLines.get(i).charAt(j+1));
+                    int sID = java.lang.Character.getNumericValue(stringLines.get(i).charAt(j+2));
+                    line.add(new Switch(j, i, null,openWeight,sID));
+                    j+=2;
+                    break;
+                case 'D':
+                    int dID = java.lang.Character.getNumericValue(stringLines.get(i).charAt(j+1));
+                    line.add(new Door(j, i, null,dID));
+                    j++;
+                    break;
+                case 'Z':
+                    zpmCount++;
+                    line.add(new NormalFloor(j, i, new Zpm(j, i)));
+                    break;
+                case 'B':
+                    line.add(new NormalFloor(j, i, new Box(j, i, 5))); // TODO change from fix 5 weight for box
+                    break;
+                case 'E':
+                    Exit newExit = new Exit(j, i);
+                    exit = newExit;
+                    line.add(newExit);
+                    break;
+                case 'P':
+                    line.add(new Pit(j, i));
+                    break;
+                case '.':
+                    line.add(new NormalFloor(j, i, null));
+                    break;
+                case '|':
+                    line.add(new Wall(j, i));
+                    break;
+                case 'L':
+                    line.add(new SpecialWall(j, i));
+                    break;
+                case 'O':
+                    Cell floor = new NormalFloor(j, i, null);
+                    this.player = new Player(floor);
+                    this.player.setField(this);
+                    line.add(floor);
+                    break;
+                }
+            }
+            cells.add(line);
+			for(List<Cell> clist : cells){
+				for(Cell value : clist){
+					if(value instanceof Door){
+						int temp = ((Door)value).getID();
+						for (List<Cell> loop2clist : cells){
+							for(Cell loop2value : loop2clist){
+								if(loop2value instanceof Switch){
+									if(((Switch)loop2value).getID()==temp){
+										((Switch)loop2value).setDoor((Door) value);
+									}
+								}
+							}
+						}
+					}
 				}
 			}
-			cells.add(line);
-		}
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public Cell getCell(int x, int y) {
-		return cells.get(y).get(x);
-	}
-
-	public void print() {
-		for (List<Cell> row : cells) {
-			for (Cell cell : row) {
-				if (cell == this.player.getPosition()) {
-					System.out.print(this.player.getImage());
-				} else {
-					System.out.print(cell.getImage());
-				}
-
-			}
-			System.out.println("");
-		}
-	}
-
-	public void loadNextLevel() {
-		level++;
-		Initialize(level);
-	}
-
-	public int getLevel() {
-		return level;
-	}
-	
-	public void addZpm(int n){
-		zpmCount += n;
-	}
-
-	public void zpmPickedUp() {
-		zpmCount--;
-		if(zpmCount <= 0){
-			exit.openExit();
-		}	
-	}
+        }
+    }
+ 
+    public int getHeight() {
+        return height;
+    }
+ 
+    public int getWidth() {
+        return width;
+    }
+ 
+    public Cell getCell(int x, int y) {
+        return cells.get(y).get(x);
+    }
+ 
+    public void print() {
+        for (List<Cell> row : cells) {
+            for (Cell cell : row) {
+                if (cell == this.player.getPosition()) {
+                    System.out.print(this.player.getImage());
+                } else {
+                    System.out.print(cell.getImage());
+                }
+ 
+            }
+            System.out.println("");
+        }
+    }
+ 
+    public void loadNextLevel() {
+        level++;
+        Initialize(level);
+    }
+ 
+    public int getLevel() {
+        return level;
+    }
+   
+    public void addZpm(int n){
+        zpmCount += n;
+    }
+ 
+    public void zpmPickedUp() {
+        zpmCount--;
+        if(zpmCount <= 0){
+            exit.openExit();
+        }  
+    }
 }
